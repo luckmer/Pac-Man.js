6a4b3e3a3b21581cf7166f6e28bc961ddb4fc0e6
luckmer <woszczo04@gmail.com>
2021-05-27 12:28:37 +0200

    global data

---
+++ src/ts/data/GlobalData.ts
@@ -0,0 +1,18 @@
+export class Global {
+  public static width: number = 28;
+  public static PlayerPosition: number = 575;
+  public static points: number = 0;
+  public static UserCenterCollider: number[] = [
+    381, 409, 351, 437, 463, 462, 461, 460, 430, 402, 374, 348, 349, 350,
+  ];
+  public static ColliderWay: string = "";
+  public static counter: number = 0;
+  public static LeftRight: boolean | string = false;
+  public static TopBottom: boolean | string = false;
+  public static GlobalPoints: number = 0;
+  public static GhostScream: boolean = false;
+  public static PlayerAttack: boolean = false;
+  public static ClearInterval: any = null;
+  public static ClearTimeOut: any = null;
+  public static Test: number = 0;
+}
